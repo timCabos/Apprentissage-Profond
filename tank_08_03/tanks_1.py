20a61c9e66aabf3d236d13712ec8592c169ee24c
TimHessel <timotheehessel@MacBook-Pro-de-Timothee.local>
2020-03-09 13:19:54 +0100

    Deux robots

--- tank_08_03/tanks_1.py
+++ tank_08_03/tanks_1.py
@@ -16,20 +16,22 @@ p_1 = pygame.image.load('sprites/p_1.png')
 p_2 = pygame.image.load('sprites/p_2.png')
 
 nb_tank = 5
-nnet_tab=[Nnet(nb_input, nb_hidden1, nb_hidden2, nb_hidden3, nb_output) for i in range(nb_tank)]
-score_tab=[0 for i in range(nb_tank)]
+nnet_tab1=[Nnet(nb_input, nb_hidden1, nb_hidden2, nb_hidden3, nb_output) for i in range(nb_tank)]
+score_tab1=[0 for i in range(nb_tank)]
+
+nnet_tab2=[Nnet(nb_input, nb_hidden1, nb_hidden2, nb_hidden3, nb_output) for i in range(nb_tank)]
+score_tab2=[0 for i in range(nb_tank)]
 
 playing_tank = 0
-game_duration = 2000
+game_duration = 1500
 time = 0
-
-print(nnet_tab[0].weight_input_hidden1)
 
 #Boucle principale
 while game < game_max:
 
     input = data(x_p1,x_p2,y_p1,y_p2,life_p1,life_p2,bullets_p1,bullets_p2)
-    output=nnet_tab[playing_tank].get_outputs(input)
+    output1=nnet_tab1[playing_tank].get_outputs(input)
+    output2=nnet_tab2[playing_tank].get_outputs(input)
 
     pygame.time.delay(delay)
     time += delay
@@ -42,34 +44,34 @@ while game < game_max:
 
 #Test des deplacements
 
-    if output[0][0]>0.5 and x_p1>0:
+    if output1[0][0]>0.5 and x_p1>0:
         x_p1 -= speed
 
-    if output[1][0]>0.5 and x_p1<window_length - width:
+    if output1[1][0]>0.5 and x_p1<window_length - width:
         x_p1 += speed
 
-    if output[2][0]>0.5 and y_p1>0:
+    if output1[2][0]>0.5 and y_p1>0:
         y_p1 -= speed
 
-    if output[3][0]>0.5 and y_p1< window_height - height:
+    if output1[3][0]>0.5 and y_p1< window_height - height:
         y_p1 += speed
 
-    if keys[pygame.K_j] and x_p2>0:
+    if output2[0][0]>0.5 and x_p2>0:
         x_p2 -= speed
 
-    if keys[pygame.K_l] and x_p2<window_length - width:
+    if output2[1][0]>0.5 and x_p2<window_length - width:
         x_p2 += speed
 
-    if keys[pygame.K_i] and y_p2>0:
+    if output2[2][0]>0.5 and y_p2>0:
         y_p2 -= speed
 
-    if keys[pygame.K_k] and y_p2< window_height - height:
+    if output2[3][0]>0.5 and y_p2< window_height - height:
         y_p2 += speed
 
 # Test du tir du joueur 1
 
     if cooldown_p1 < 0:
-        if output[4][0]>0.5 or output[5][0]>0.5 or output[6][0]>0.5 or output[7][0]>0.5 :
+        if output1[4][0]>0.5 or output1[5][0]>0.5 or output1[6][0]>0.5 or output1[7][0]>0.5 :
             i = 0
             while bullets_p1[i][0] :
                 i += 1
@@ -78,13 +80,13 @@ while game < game_max:
             bullets_p1[i][2] = y_p1
             bullets_p1[i][3] = 0
             bullets_p1[i][4] = 0
-            if output[4][0]>0.5 :
+            if output1[4][0]>0.5 :
                 bullets_p1[i][3] = -bullet_speed
-            if output[5][0]>0.5 :
+            if output1[5][0]>0.5 :
                 bullets_p1[i][3] = bullet_speed
-            if output[6][0]>0.5 :
+            if output1[6][0]>0.5 :
                 bullets_p1[i][4] = -bullet_speed
-            if output[7][0]>0.5 :
+            if output1[7][0]>0.5 :
                 bullets_p1[i][4] = bullet_speed
             cooldown_p1 = cooldown_delay
 
@@ -94,7 +96,7 @@ while game < game_max:
 #Test du tir du joueur 2
 
     if cooldown_p2 < 0:
-        if keys[pygame.K_f] or keys[pygame.K_h] or keys[pygame.K_t] or keys[pygame.K_g]:
+        if output2[4][0]>0.5 or output2[5][0]>0.5 or output2[6][0]>0.5 or output2[7][0]>0.5 :
             i = 0
             while bullets_p2[i][0] :
                 i += 1
@@ -103,13 +105,13 @@ while game < game_max:
             bullets_p2[i][2] = y_p2
             bullets_p2[i][3] = 0
             bullets_p2[i][4] = 0
-            if keys[pygame.K_f]:
+            if output2[4][0]>0.5 :
                 bullets_p2[i][3] = -bullet_speed
-            if keys[pygame.K_h]:
+            if output2[5][0]>0.5 :
                 bullets_p2[i][3] = bullet_speed
-            if keys[pygame.K_t]:
+            if output2[6][0]>0.5 :
                 bullets_p2[i][4] = -bullet_speed
-            if keys[pygame.K_g]:
+            if output2[7][0]>0.5 :
                 bullets_p2[i][4] = bullet_speed
             cooldown_p2 = cooldown_delay
 
@@ -132,9 +134,10 @@ while game < game_max:
                 life_p2 -= 1
                 if life_p2 == 0 :
                     game += 1
-                    score = (life_p1-life_p2)/life_tot * (1 - time/game_duration)
+                    score = (life_p1-life_p2)/life_tot * (1 - time/(2*game_duration))
                     print(score)
-                    score_tab[playing_tank]=score
+                    score_tab1[playing_tank]=score
+                    score_tab2[playing_tank]=-score
                     playing_tank = (playing_tank + 1)
                     life_p1 = life_tot
                     life_p2 = life_tot
@@ -159,9 +162,10 @@ while game < game_max:
                 life_p1 -= 1
                 if life_p1 == 0 :
                     game += 1
-                    score = (life_p1-life_p2)/life_tot * (1 - time/game_duration)
+                    score = (life_p1-life_p2)/life_tot * (1 - time/(2*game_duration))
                     print(score)
-                    score_tab[playing_tank]=score
+                    score_tab1[playing_tank]=score
+                    score_tab2[playing_tank]=-score
                     playing_tank = (playing_tank + 1)
                     life_p1 = life_tot
                     life_p2 = life_tot
@@ -181,7 +185,8 @@ while game < game_max:
         game += 1
         score = (life_p1-life_p2)/life_tot * (1 - time/(2*game_duration))
         print(score)
-        score_tab[playing_tank]=score
+        score_tab1[playing_tank]=score
+        score_tab2[playing_tank]=-score
         playing_tank = (playing_tank + 1)
         life_p1 = life_tot
         life_p2 = life_tot
@@ -195,14 +200,14 @@ while game < game_max:
 
     if playing_tank == nb_tank :
         playing_tank = 0
-        print(score_tab)
-        print(nnet_tab)
-        good_nn, bad_nn = darwin(nnet_tab, score_tab)
-        print(good_nn)
-        print(bad_nn)
-        bad_nn = sort_bad(bad_nn)
-        child1 = breed(good_nn[0], good_nn[1])
-        child2 = breed(good_nn[0], good_nn[1])
-        nnet_tab = [good_nn[0], good_nn[1], bad_nn[0], bad_nn[1], child1]
+        print(score_tab1)
+        good_nn1, bad_nn1 = darwin(nnet_tab1, score_tab1)
+        good_nn2, bad_nn2 = darwin(nnet_tab2, score_tab2)
+        bad_nn1 = sort_bad(bad_nn1)
+        bad_nn2 = sort_bad(bad_nn2)
+        child1 = breed(good_nn1[0], good_nn1[1])
+        child2 = breed(good_nn2[0], good_nn2[1])
+        nnet_tab1 = [good_nn1[0], good_nn1[1], bad_nn1[0], bad_nn1[1], child1]
+        nnet_tab2 = [good_nn2[0], good_nn2[1], bad_nn2[0], bad_nn2[1], child2]
     pygame.display.update()
 pygame.quit()
